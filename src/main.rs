b3baaeaaf246da3a950a7d12697a8c510182770d
TerraDev64 <TerraDev64@gmail.com>
2021-01-13 20:10:55 +0100

    shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 3;
+    println!("The value of x is: {}", x);
+
+    let y = 5;
+    let y = y + 1;
+    let y = y * 2;
+    println!("The value of x is: {}", y);
 }
