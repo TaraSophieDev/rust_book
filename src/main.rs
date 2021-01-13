cc17cda772db4a4c69fb77ea13eec6a7f269b75e
TerraDev64 <TerraDev64@gmail.com>
2021-01-13 20:13:29 +0100

    used functions to make the code easier to read

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 3;
-    println!("The value of x is: {}", x);
+    
+}
 
+fn shadowing() {
     let y = 5;
     let y = y + 1;
     let y = y * 2;
     println!("The value of x is: {}", y);
+}
+
+fn mutable_variables() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 3;
+    println!("The value of x is: {}", x);    
 }
